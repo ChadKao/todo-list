873796b79fbe37893d3d4d6d45cec81c7418dcf5
ChadKao <coolchad2000@gmail.com>
2023-11-27 17:23:34 +0800

    fea: add login and register page

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,13 @@ const User = db.User;
 
 router.get('/', (req, res) => { res.redirect('/users/login') });
 
-router.get('/register', (req, res) => { res.send('register') });
+router.get('/register', (req, res) => { res.render('register') } ); ;
 
-router.post('/register', (req, res) => { res.send('register') });
+router.post('/register', (req, res) => { res.send(req.body) });
 
-router.get('/login', (req, res) => { res.send('login') });
+router.get('/login', (req, res) => { res.render('login') });
 
-router.post('/login', (req, res) => { res.send('login') });
+router.post('/login', (req, res) => { res.send(req.body) });
 
 router.post('/logout', (req, res) => { res.send('logout') });
 
