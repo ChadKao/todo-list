b4e86d699be19c771790e65727ef4ba6d288a635
ChadKao <coolchad2000@gmail.com>
2023-11-27 20:56:50 +0800

    feat: implement user registration route and logic
    This commit adds the registration route in 'routes/todo.js' with the corresponding logic to handle user registration. It validates the input data, checks for matching passwords, and ensures that the email is not already registered. In case of success, the user is redirected to the login page. Error handling is also implemented, providing appropriate flash messages for different scenarios."

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,41 @@ const User = db.User;
 
 router.get('/', (req, res) => { res.redirect('/users/login') });
 
-router.get('/register', (req, res) => { res.render('register') } ); ;
+router.get('/register', (req, res) => { res.render('register') });;
 
-router.post('/register', (req, res) => { res.send(req.body) });
+router.post('/register', (req, res, next) => {
+  const { name, email, password, 'confirm-password': confirmPassword } = req.body
+
+  if (!email || !password || !confirmPassword) {
+    req.flash('error', 'email和密碼都是必填')
+    return res.redirect('back')
+  }
+
+  if (password !== confirmPassword) {
+    req.flash('error', '密碼與驗證密碼不相符')
+    return res.redirect('back')
+  }
+
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        req.flash('error', '信箱已被註冊')
+        return res.redirect('back')
+      }
+      return User.create({ name, email, password })
+        .then(() => {
+          req.flash('success', '註冊成功')
+          res.redirect('/users/login')
+        })
+        .catch(error => {
+          error.errorMessage = '註冊失敗'
+          next(error)
+        })
+    })
+    .catch(error => {
+      next(error)
+    })
+});
 
 router.get('/login', (req, res) => { res.render('login') });
 
