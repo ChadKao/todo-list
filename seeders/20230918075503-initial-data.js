f2ab07adc9dcd3235717d92cf359ad089f7e6761
ChadKao <coolchad2000@gmail.com>
2024-01-08 15:19:53 +0800

    chore: update seeders with users data

--- seeders/20230918075503-initial-data.js
+++ seeders/20230918075503-initial-data.js
@@ -1,20 +1,46 @@
 'use strict';
+const bcrypt = require('bcryptjs')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('todos', 
-      Array.from({length: 10}).map((_, i) => 
+  async up(queryInterface, Sequelize) {
+    let transaction
+    try {
+      const hash = await bcrypt.hash('123', 10)
+      transaction = await queryInterface.sequelize.transaction()
+
+      await queryInterface.bulkInsert('users', [
+        {
+          id: 1,
+          name: 'root',
+          email: 'user1@example.com',
+          password: hash,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ], { transaction })
+
+
+      await queryInterface.bulkInsert('todos',
+        Array.from({ length: 10 }).map((_, i) =>
         ({
           name: `todo-${i}`,
+          userId: 1,
           createdAt: new Date(),
           updatedAt: new Date()
         })
+        ),
+        { transaction }
       )
-    )
+
+      await transaction.commit()
+    } catch (error) {
+      if (transaction) transaction.rollback()
+      console.error(error)
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('todos',null)
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null)
   }
 };
